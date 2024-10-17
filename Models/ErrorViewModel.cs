73803850fdd7e1d0623d9b6ab5055cef3787e51b
ST10100378 <ST10100378@rcconnect.edu.za>
2024-10-17 08:48:54 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kabelo_Monageng_Portfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
